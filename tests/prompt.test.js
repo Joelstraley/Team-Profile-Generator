2da95e26c2244c33c994991924cbf7d8a5930562
Joel Straley <joelstraley@gmail.com>
2021-01-26 15:51:36 -0500

    updated classes

---
+++ tests/prompt.test.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+const Employee = require("./employee");
+const Manager = require("./manager");
+const Engineer = require("./engineer");
+const Intern = require("./intern");
+
+
+describe("", () => {
+  it("Creates an array of Letter objects", () => {
+    const word = new Word("hi");
